0557a3c5290f4c34e2113c2822009075502dc468
Iris The Fox <iris-miz@yandex.ru>
2019-03-01 17:23:57 +0300

    add friendly id

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
-	has_many :products, dependent: :nullify
-	has_ancestry orphan_strategy: :adopt
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  has_many :products, dependent: :nullify
+  has_ancestry orphan_strategy: :adopt
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  extend FriendlyId
   belongs_to :category
+  friendly_id :title, use: :slugged
 end
