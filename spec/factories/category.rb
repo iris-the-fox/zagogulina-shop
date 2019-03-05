15427f876fd9e39ecbfb4cee07f7aa550667d84c
Iris The Fox <iris-miz@yandex.ru>
2019-03-05 13:24:37 +0300

    add specs for products controller

---
+++ spec/factories/category.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :category, class: Category do
+
+    title {"sometitle"}
+    id {1}
+    
+  end
+
+end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -14,4 +14,10 @@ module ControllerMacros
       sign_in user
     end
   end
+
+  def create_category
+    before(:each) do
+      category = FactoryBot.create(:category)
+    end
+  end
 end
